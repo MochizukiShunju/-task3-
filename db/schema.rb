6d9ea75a9d0e2862aa8cb87ca78c35e674cf84e8
MochizukiShunju <b1302474@gmail.com>
2021-01-16 12:38:20 +0000

    [Update]task3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_025248) do
+ActiveRecord::Schema.define(version: 2021_01_16_113030) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2021_01_16_025248) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "introduction"
+    t.string "profile_image_id"
+    t.string "title"
+    t.string "opinion"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
